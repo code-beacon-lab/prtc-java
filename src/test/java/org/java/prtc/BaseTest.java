521a8fd4a74d5591ba5e3444fc5d54d96a55c49b
danny <gryb809@gmail.com>
2022-05-02 19:13:57 +0900

    :construction_worker: gradle 적용 및 java version 변경

---
+++ src/test/java/org/java/prtc/BaseTest.java
@@ -0,0 +1,12 @@
+package org.java.prtc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BaseTest {
+    @Test
+    void test() {
+        assertThat("").isEqualTo("");
+    }
+}
